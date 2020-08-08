de138a8c376a46059f7e4002d4c6138318e87fc7
Олег Есиков <esikov.oleg@mail.ru>
2020-08-08 22:40:10 +0300

    Обновление интерфейса. Добавление заглушек

--- ConwaysGameOfLife/MainWindow.xaml.cs
+++ ConwaysGameOfLife/MainWindow.xaml.cs
@@ -22,6 +22,9 @@ namespace ConwaysGameOfLife
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int widthMap = 10;
+        private int heightMap = 10;
+        private int cellSize = 30;
         private Game Game;
 
         public MainWindow()
@@ -31,44 +34,24 @@ namespace ConwaysGameOfLife
             {
                 StartInfoPanel.Children.Clear();
                 ToolBar.Visibility = Visibility.Visible;
-                CreateMap(15, 10, 30);
+                CreateMap(widthMap, heightMap, cellSize);
             };
             StartGameButton.Click += (sender, e) =>
             {
                 StartGameButton.IsEnabled = false;
+                SaveMapButton.IsEnabled = false;
+                LoadMapButton.IsEnabled = false;
                 Map.IsHitTestVisible = false;
                 Game.Start();
             };
+            SaveMapButton.Click += (sender, e) => throw new NotImplementedException();
+            LoadMapButton.Click += (sender, e) => throw new NotImplementedException();
+            SizeMenu.AddHandler(RadioButton.CheckedEvent, new RoutedEventHandler(RadioButtonSizeChecked));
         }
 
         private void CreateMap(int width, int height, int cellSize)
         {
             var beginState = Game.CreateState(width, height);
-            beginState[3, 3].State = Brushes.Black;
-            beginState[3, 4].State = Brushes.Black;
-            beginState[3, 5].State = Brushes.Black;
-            beginState[3, 6].State = Brushes.Black;
-            beginState[3, 7].State = Brushes.Black;
-            beginState[4, 3].State = Brushes.Black;
-            beginState[4, 4].State = Brushes.Black;
-            beginState[4, 5].State = Brushes.Black;
-            beginState[4, 6].State = Brushes.Black;
-            beginState[4, 7].State = Brushes.Black;
-            beginState[5, 3].State = Brushes.Black;
-            beginState[5, 4].State = Brushes.Black;
-            beginState[5, 5].State = Brushes.Black;
-            beginState[5, 6].State = Brushes.Black;
-            beginState[5, 7].State = Brushes.Black;
-            beginState[6, 3].State = Brushes.Black;
-            beginState[6, 4].State = Brushes.Black;
-            beginState[6, 5].State = Brushes.Black;
-            beginState[6, 6].State = Brushes.Black;
-            beginState[6, 7].State = Brushes.Black;
-            beginState[7, 3].State = Brushes.Black;
-            beginState[7, 4].State = Brushes.Black;
-            beginState[7, 5].State = Brushes.Black;
-            beginState[7, 6].State = Brushes.Black;
-            beginState[7, 7].State = Brushes.Black;
             Game = new Game(beginState);
 
             for (int j = 0; j < height; j++)
@@ -104,6 +87,15 @@ namespace ConwaysGameOfLife
                     shape.SetBinding(Shape.FillProperty, binding);
                 }
             }
+        }
+
+        private void RadioButtonSizeChecked(object sender, RoutedEventArgs e)
+        {
+            var pressed = (RadioButton)e.Source;
+            var text = pressed.Content.ToString();
+            var data = text.Split('x');
+            widthMap = int.Parse(data[0]);
+            heightMap = int.Parse(data[1]);
         }
     }
 }
