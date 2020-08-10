51de935d893d71ffed8197862e60094bb75a5e48
Олег Есиков <esikov.oleg@mail.ru>
2020-08-10 23:03:19 +0300

    Обновление меню выбора размера поля

--- ConwaysGameOfLife/SizeMap.cs
+++ ConwaysGameOfLife/SizeMap.cs
@@ -4,8 +4,8 @@
     {
         public int WidthCellCount { get; private set; }
         public int HeightCellCount { get; private set; }
-        public string WidthStr { get => $"Ширина карты: {WidthCellCount}"; }
-        public string HeightStr { get => $"Высота карты: {HeightCellCount}"; }
+        public string WidthStr { get => $": {WidthCellCount}"; }
+        public string HeightStr { get => $": {HeightCellCount}"; }
 
         public SizeMap(int widthCellCount, int heightCellCount)
         {
